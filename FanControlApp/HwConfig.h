708e2403389f47701b4671884c23c0ab3c738969
Sanpee <59129423+Zeephdek@users.noreply.github.com>
2021-07-20 20:43:12 +0800

    Fixed wrong pin num for RX Data.

--- FanControlApp/HwConfig.h
+++ FanControlApp/HwConfig.h
@@ -13,15 +13,11 @@
   #define OLED_DC     18
   #define OLED_CS     17
   #define LED         16
-  
-  U8G2_SH1106_128X64_NONAME_1_4W_SW_SPI u8g2(U8G2_R0, OLED_SCK, OLED_SDA, OLED_CS, OLED_DC, OLED_RES );
   #define DISP_INIT() u8g2.begin() 
-  
-  
-
+  U8G2_SH1106_128X64_NONAME_1_4W_SW_SPI u8g2(U8G2_R0, OLED_SCK, OLED_SDA, OLED_CS, OLED_DC, OLED_RES );
 #endif
 
 #define RF_TX_DATA 27   // Heltec WIFI Kit32
-#define RF_RX_DATA 32   // Heltec WIFI Kit32
+#define RF_RX_DATA 35   // Heltec WIFI Kit32
 
 #endif // _HWCONFIG_H_
